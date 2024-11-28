0cd89af55296c5444e68e2c37b8c8ae169c63427
yhtttt050723 <519275808@qq.com>
2024-11-28 14:25:42 +0800

    对所有功能函数完善和菜单概要

--- 线性表实现.c
+++ 线性表实现.c
@@ -100,17 +100,17 @@ void ListInsert (SeqList *list)//在position处插入元素，使线性表长度加一
 	int position;
 	int value;
 	int i;
-	while (1)
+	for (i = 0; i < MAX_SIZE ;i++)
 	{
-	printf("请输入要插入元素的位置：(输入-1终止输入)\n");
+		if (position == -2)
+		{
+			break;
+		}
+	printf("请输入要插入元素的位置：(输入-2终止输入)\n");
 	scanf("%d",&position);
 	printf("请输入要插入元素的值：\n");
 	scanf("%d",&value);
 
-	if (position == -1)
-	{
-		break;
-	} 
 	if (position < 0||position > list-> length||position >= MAX_SIZE)//对position位置进行判断 
 	{
 		printf("输入无效，位置无效或者列表已满\n");
@@ -124,32 +124,45 @@ void ListInsert (SeqList *list)//在position处插入元素，使线性表长度加一
 	list->elem[position] = value;
 	list->length++;
 	printf("元素已经成功插入\n");
-	break;
 	}
 	}
-}  //插入在position处插入value 
+}//插入在position处插入value
+   
 	
 
 
-int ListDelete (SeqList *list,int position ,int *value)
+void ListDelete (SeqList *list)
 {
+	int position;
+	int *value;
+	int i;
+	int j;
+	for (j = 0 ; j < MAX_SIZE ;j++)
+{
+		printf("请输入要删除元素的位置(输入-3结束删除)\n");
+		scanf("%d",&position); 
 	if (position < 0||position > list-> length||position >= MAX_SIZE)//对position位置进行判断 
 	{
-		return -1;
+		break;
 	}
-	*value = list->arr[position-1];
-	int i;
-	for ( i = position - 1;i < position;i++)//将position位置前元素前移 
+	if (position == -3)
 	{
-		list->arr[i] = list->arr[i+1];	
+		break;
+	}
+		for ( i = position - 1;i < list->length-1 ; ++i)//将position位置前元素前移 
+	{
+		list->elem[i] = list->elem[i+1];	
 	}
 	list->length--;
-	return 0 ;
+	printf("删除成功\n");
+}
+	//break ;
 }//删除position处元素 
 
 void printList(SeqList *list)
 {
 	int i;
+	printf("线性表如下\n"); 
 	for (i=0 ; i<list->length ;i++)
 	{
 		printf("%d\n",list->elem[i]);
@@ -158,43 +171,84 @@ void printList(SeqList *list)
 	printf("\n");
 } //打印线性表 
 
-int GetElem (SeqList *list,int position ,int *value)
+void GetElem (SeqList *list)
 {
-	if (position < 1 || position > list->length)
+	int position;
+	int value;
+	int i,j;
+	for (i = 0; i < list->length ; i++ )
 	{
-		return -1;//返回失败的返回值 
+		if (position == -1)
+		{
+			break;
+		}
+		printf("请输入要获取元素的位置(输入-1结束获取)\n");
+		scanf("%d",&position);
+		value = list->elem[position-1];
+		if (position < 1 || position > list->length)
+	{
+		printf("输入位置无效\n");
+		break;
 	}
-	*value = list->arr[position - 1];
-	return 0;//返回成功的返回值 
+	printf("该位置元素为%d\n",value);
+	}
 }  //获取position处的值 
 
-int PriorElem(SeqList *list,int position,int *value)
+void PriorElem(SeqList *list)
 {
+	int position,value;
+	int i;
+	for (i = 0 ; i < list->length; i++)
+	{	
+	printf("请输入要获取前驱元素的位置\n");
+	scanf("%d",&position); 
 	if (position < 2 || position > list->length)
 	{
-		return -1;
+		break;
 	}
-	*value = list->arr[position - 2];
-	return 0 ;
+	if (position == -1)
+	{
+		break;
+	}
+	value = list->elem[position - 2];
+	printf("该前驱元素为%d\n",value);
+	}
+
 } //获取前驱元素 
 
-int NextElem(SeqList *list,int position,int *value)
+void NextElem(SeqList *list)
 {
+	int position,value;
+	int i;
+	for (i = 0 ; i < list->length; i++)
+	{	
+	printf("请输入要获取后继元素的位置\n");
+	scanf("%d",&position); 
 	if (position < 0 || position > list->length)
 	{
-		return -1;
+		break;
 	}
-	*value = list->arr[position];
-	return 0 ;
+	if (position == -1)
+	{
+		break;
+	}
+	value = list->elem[position];
+	printf("该后继元素为%d\n",value);
+	}
+
 }//获取后继元素 
 
 
 
 void Menu()
 {
+	int choice;
+	SeqList list;
+	int i;
 	printf("===================线性表基础操作=============\n"); 
+	printf("===================0.退出菜单=================\n"); 
 	printf("===================1.创建线性表===============\n"); 
-	printf("===================2.在指定位置插入元素=======\n"); 
+	printf("===================2.按位置插入元素===========\n"); 
 	printf("===================3.获取指定元素=============\n"); 
 	printf("===================4.获取前驱元素=============\n"); 
 	printf("===================5.获取后继元素=============\n"); 
@@ -202,18 +256,37 @@ void Menu()
 	printf("===================7.打印线性表===============\n"); 
 	printf("===================8.检查表是否为空表=========\n"); 
 	printf("===================9.销毁线性表===============\n"); 
-	printf("===================0.退出菜单=================\n"); 
+	printf("===================10.按指定位置插入元素======\n"); 
 	printf("===================线性表基础操作=============\n"); 
 	printf("===================线性表基础操作=============\n"); 
-	printf("===================线性表基础操作=============\n"); 
-	printf("===================线性表基础操作=============\n"); 
+	printf("===================线性表基础操作=============\n");
+for (i = 0 ; i < 100000000 ; i++)
+	{
+		scanf("请输入对线性表的操作\n",&choice);
+	switch(choice)
+		{
+			case 1:InitList(&list);
+			case 2:Insert(&list);
+			case 3:GetElem(&list);
+			case 4:PriorElem(&list);
+			case 5:NextElem(&list);
+			case 6:ListLength(&list);
+			case 7:printList(&list);
+			case 8:ListEmpty(&list);
+			case 9:DestroyList(&list);
+			case 10:ListInsert(&list);
+			case 0: exit(0) ; //退出系统
+		}	
+	} 
 } 
 
 int main (void)
-{
+{	 
+	Menu();
+}
 
-	 
-	SeqList list;
+
+
 /*	int i;
 	while(1)
 	{
@@ -283,14 +356,18 @@ int main (void)
 	
 	printf("%d\n",e);
 	printf("%d\n",pre_e);
-	printf("%d\n",next_e);*/
+	printf("%d\n",next_e);
 	InitList (&list);
 	Insert(&list);
 	ListInsert(&list);
 	ListEmpty(&list);
 	ListLength(&list);
+	GetElem(&list);
+	PriorElem(&list);
+	NextElem(&list); 
 	printList(&list);
-	DestroyList(&list);
+	ListDelete(&list);
+	printList(&list);
+	DestroyList(&list);*/
 	
 
-}
